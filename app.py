101df1c1cf53aa1ab35075179dde5c95b21c7831
Chuck-2022 <charles.fortier2000@gmail.com>
2026-04-04 22:08:18 -0400

    missing requirements

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
-import random
 from database import *
-import time
 from common import *
 import pandas as pd
 from apscheduler.schedulers.background import BackgroundScheduler
