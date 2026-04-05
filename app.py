6b708276d3eac0070919ecda4407c04539c98265
Chuck-2022 <charles.fortier2000@gmail.com>
2026-04-04 22:03:59 -0400

    code 137

--- app.py
+++ app.py
@@ -133,4 +133,7 @@ if __name__ == '__main__':
     scheduler.start()
 
     # app.run(debug=True, host="127.0.0.1")
-    app.run(host="0.0.0.0")
+    try:
+        app.run(host="0.0.0.0")
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
